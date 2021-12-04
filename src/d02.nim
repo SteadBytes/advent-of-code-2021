6b39e6ae79f4308521be63a1ae17e8be763c06e0
Ben Steadman <steadmanben1@gmail.com>
2021-12-04 12:56:20 +0000

    d02 p02
    Signed-off-by: Ben Steadman <steadmanben1@gmail.com>

--- src/d02.nim
+++ src/d02.nim
@@ -1,23 +1,42 @@
 import sequtils, sugar, strutils
 
-proc part1(input: string): int =
-  let (x, y) =
+proc parseInput(input: string): seq[(string, int)] =
     input
     .strip()
     .splitLines()
     .map(s => (let parts = s.split(maxsplit = 1); (parts[0], parseInt(parts[1]))))
-    .foldl(
-      (let
-        (x, y) = a
-        (d, n) = b
-      case d:
-        of "forward": (x + n, y)
-        of "down": (x, y + n)
-        of "up": (x, y - n)
-        else: raise newException(ValueError, "Invalid direction: " & d)),
-      (0, 0))
-  x * y
+
+proc part1(instructions: seq[(string, int)]): int =
+    let (x, y) =
+        instructions
+        .foldl(
+          (let
+            (x, y) = a
+            (d, n) = b
+        case d:
+            of "forward": (x + n, y)
+            of "down": (x, y + n)
+            of "up": (x, y - n)
+            else: raise newException(ValueError, "Invalid direction: " & d)),
+          (0, 0))
+    x * y
+
+proc part2(instructions: seq[(string, int)]): int =
+    let (x, y, _) =
+        instructions
+        .foldl(
+          (let
+            (x, y, aim) = a
+            (d, n) = b
+        case d:
+            of "forward": (x + n, y + aim * n, aim)
+            of "down": (x, y, aim + n)
+            of "up": (x, y, aim - n)
+            else: raise newException(ValueError, "Invalid direction: " & d)),
+          (0, 0, 0))
+    x * y
 
 const input = staticRead("../inputs/d02.txt")
-echo "part 1: ", part1(input)
-# echo "part 2: "
+let instructions = parseInput(input)
+echo "part 1: ", part1(instructions)
+echo "part 2: ", part2(instructions)
