a3245d889f27e9e9c08542bd612c08129f24e7f0
Ben Steadman <steadmanben1@gmail.com>
2021-12-06 18:10:09 +0000

    d04 p01 initial
    Signed-off-by: Ben Steadman <steadmanben1@gmail.com>

---
+++ src/d04.nim
@@ -0,0 +1,69 @@
+{.experimental: "caseStmtMacros".}
+import strutils, sequtils, sugar, tables, options, fusion/matching
+
+
+# Since the chosen numbers are provided up front, the winning board can be
+# deduced without turn-by-turn simulation of the game:
+# - The list of chosen numbers provide a mapping from number -> turn
+# - The turn at which a row/column is completed is the maximum value given by
+#   the number -> turn mapping for the numbers in that row/column
+# - The first row/column to be completed for a given board is the minimum completion turn for all it's rows/columns
+# - The winning board is the board with the minimum turn for fist row/column completion
+#
+# The unmarked values for the winning board are those with a corresponding turn
+# greater than the winning turn.
+
+# TODO: move these to utils
+proc filterMap[T](s: openArray[T]; f: proc (x: T): Option[T]): seq[T] =
+  s.map(f).filter(x => x.isSome).map(x => x.get())
+
+proc get[A, B](t: Table[A, B]; key: A): Option[B] =
+  if t.hasKey(key):
+    some(t[key])
+  else:
+    none(B)
+
+proc winningTurn(turnsMap: Table[int, int]; board: seq[seq[int]]): Option[int] =
+  let turns = board.map(l => l.filterMap(x => turnsMap.get(x)))
+  let rows = turns.filter(r => r.len == 5)
+  let rowWin = if rows.len > 0: some(rows.map(r => r.max).min) else: none(int)
+  let cols = collect:
+    for i in 0..rows.len-1:
+      rows.map(r => r[i])
+  let colWin = if cols.len > 0: some(cols.map(c => c.max).min) else: none(int)
+  result = case (rowWin, colWin):
+    of (Some(@a), Some(@b)): some(min(a, b))
+    of (Some(@a), None()): some(a)
+    of (None(), Some(@b)): some(b)
+    else: none(int)
+
+
+const input = staticRead("../inputs/d04.txt")
+
+let sections = input.split("\n\n")
+let numbers = sections[0].split(",").map(parseInt)
+# I _wish_ iterators were more composable e.g. numbers.pairs.map(x => (x[1], x[0]))
+let turnsMap = collect:
+  for i, x in numbers: {x: i}
+# TODO: Use arrays here as we know each board is 5x5?
+let boards = sections[1..^1].map(s => s.strip().splitLines().map(l =>
+    l.splitWhitespace().map(parseInt)))
+let winTurns = boards.map(b => winningTurn(turnsMap, b))
+let winIdx = winTurns.map(x => x.get(high int)).minIndex()
+# Sanity check 
+assert winTurns[winIdx].isSome
+let finalTurn = winTurns[winIdx].get()
+let finalNumber = numbers[finalTurn]
+let unmarkedSum =
+  boards[winIdx]
+    .map(l => l.map(x => (
+      if turnsMap.getOrDefault(x, high int) <= finalTurn:
+      0
+    else:
+      x
+    )).foldl(a + b))
+    .foldl(a + b)
+let score = unmarkedSum * finalNumber
+
+echo "part 1: ", score
+#echo "part 2: "
