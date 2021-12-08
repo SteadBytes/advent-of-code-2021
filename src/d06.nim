6480eba1125602f5addfdc66e347e6a6b4b794e5
Ben Steadman <steadmanben1@gmail.com>
2021-12-08 19:26:50 +0000

    d06 p02
    Signed-off-by: Ben Steadman <steadmanben1@gmail.com>

--- src/d06.nim
+++ src/d06.nim
@@ -1,20 +1,25 @@
-import strutils, sequtils
+import strutils, sequtils, algorithm
+
+proc countFish(ages: seq[int], days: int): int =
+  # Mapping from timer value to number of fish with that timer value
+  var timerCounts: array[0..8, int]
+  for x in ages:
+    timerCounts[x].inc
+  # Each day, the number of fish for each timer value "shifts left":
+  # - The timer for each fish with timer values [1..8] decrements
+  # - Timer value 8 takes on the value of timer 0 to represent the new fish -
+  # - Timer value 6 increases by timer value 0 to account for the parents of the
+  #   new fish (now restarting their cycle)
+  for _ in 0..<days:
+    timerCounts.rotateLeft(1)
+    timerCounts[6] += timerCounts[8]
+  timerCounts.foldl(a + b)
 
 proc main() =
   const input = staticRead("../inputs/d06.txt")
-  var ages = input.strip().split(",").map(parseInt)
-  for _ in 0..<80:
-    var new = 0
-    for x in ages.mitems:
-      if x == 0:
-        x = 6
-        inc new
-      else:
-        dec x
-    ages &= repeat(8, new)
-
-  echo "part 1: ", ages.len
-  #echo "part 2: "
+  let ages = input.strip().split(",").map(parseInt)
+  echo "part 1: ", countFish(ages, 80)
+  echo "part 2: ", countFish(ages, 256)
 
 when isMainModule:
   main()
