a1483f3f81bb7e7a06001aa141d80bb99ff53bfb
Ben Steadman <steadmanben1@gmail.com>
2021-12-12 13:32:40 +0000

    d10 p02
    Signed-off-by: Ben Steadman <steadmanben1@gmail.com>

--- src/aoc.nim
+++ src/aoc.nim
@@ -1,8 +1,23 @@
-import strutils, sequtils
+import strutils, sequtils, options, sugar, tables
+
+# Puzzle input parsing
 
 proc readInputInts*(path: string): seq[int] {.compiletime.} =
   ## Compile-time parse an integer per line from puzzle input at `path`.
   return staticRead(path).strip().splitLines().map(parseInt)
+
+# `Option` utilities
+
+proc filterMap*[T, S](s: openArray[T]; f: proc (x: T): Option[S]): seq[S] =
+  s.map(f).filter(x => x.isSome).map(x => x.get())
+
+proc get*[A, B](t: Table[A, B]; key: A): Option[B] =
+  if t.hasKey(key):
+    some(t[key])
+  else:
+    none(B)
+
+# Misc
 
 template unreachable*(): untyped =
   raise newException(Exception, "internal error: entered unreachable code")
--- src/d10.nim
+++ src/d10.nim
@@ -1,11 +1,15 @@
-import strutils, sequtils, tables
+import aoc
+import strutils, sequtils, tables, options, algorithm
 
 # TODO: Would `case` be more efficient or does Nim optimise these constant
 # table lookups into something equivalent?
 const pairs = {'(': ')', '[': ']', '{': '}', '<': '>'}.toTable()
 const scores = {')': 3, ']': 57, '}': 1197, '>': 25137}.toTable()
+const completionScores = {')': 1, ']': 2, '}': 3, '>': 4}.toTable()
 
-func syntaxErrorScore(line: string): int = 
+# TODO: Is there a useful form of these two that reduces duplication?
+
+func syntaxErrorScore(line: string): int =
   var stack: seq[char]
   for c in line:
     if c in pairs:
@@ -15,11 +19,33 @@ func syntaxErrorScore(line: string): int =
     else:
       return scores[c]
 
+func correctionScore(line: string): Option[int] =
+  var stack: seq[char]
+  for c in line:
+    if c in pairs:
+      stack.add(c)
+    elif c == pairs[stack[^1]]:
+      discard stack.pop()
+    else:
+      # Corrupted line
+      return none(int)
+  # Valid line
+  if stack.len == 0:
+    return none(int)
+  # Incomplete line
+  # Why does `foldl` have an implementation with a starting value but `foldr` does not?!
+  # some(stack.foldr(a * 5 + completionScores[pairs[b]], 0))
+  some(stack.reversed().foldl(a * 5 + completionScores[pairs[b]], 0))
+
+
 proc main() =
   const input = staticRead("../inputs/d10.txt")
+  let lines = input.strip().splitLines()
 
-  echo "part 1: ", input.strip().splitLines().map(syntaxErrorScore).foldl(a + b)
-  #echo "part 2: "
+  echo "part 1: ", lines.map(syntaxErrorScore).foldl(a + b)
+
+  let scores = lines.filterMap(correctionScore)
+  echo "part 2: ", scores.sorted()[scores.len div 2]
 
 when isMainModule:
   main()
