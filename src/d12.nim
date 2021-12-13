18a45a5b6953e5e69fc5825a07531f3bb0817ab9
Ben Steadman <steadmanben1@gmail.com>
2021-12-13 20:33:07 +0000

    d12 p02 initial
    Signed-off-by: Ben Steadman <steadmanben1@gmail.com>

--- src/d12.nim
+++ src/d12.nim
@@ -9,6 +9,42 @@ func parseConnection(s: string): (string, string) =
 # finding _all_ paths through the graph would be NP hard due to it not being
 # acyclic.
 
+func part1(g: Table[string, seq[string]]): int =
+  # Basic DFS to find all paths through the graph. There's a lot of
+  # string/array copying going on here which could be optimised by not
+  # referencing nodes with strings but this works and executes extremely
+  # quickly anyway.
+  var stack = @[@["start"]]
+  while stack.len > 0:
+    let path = stack.pop()
+    if path[^1] == "end":
+      # Complete path found
+      inc result
+    else:
+      # Continue exploring path to adjacent big caves or not previously
+      # explored small caves
+      for n2 in g[path[^1]]:
+        if n2[0].isUpperAscii() or n2 notin path:
+          stack.add(path & n2)
+
+func part2(g: Table[string, seq[string]]): int =
+  var stack = @[(@["start"], true)]
+  while stack.len > 0:
+    let (path, canRevisit) = stack.pop()
+    if path[^1] == "end":
+      # Complete path found
+      inc result
+    else:
+      # Always explore adjacent big caves or not previously explored small
+      # caves
+      for n2 in g[path[^1]]:
+        if n2[0].isUpperAscii() or n2 notin path:
+          stack.add((path & n2, canRevisit))
+        # Note: Visiting end only once is already handled above when a
+        # complete path is found
+        elif canRevisit and n2 != "start":
+          stack.add((path & n2, false))
+
 proc main() =
   const input = staticRead("../inputs/d12.txt")
   let g =
@@ -19,26 +55,8 @@ proc main() =
         g.mgetOrPut(y, @[]).add(x)
       g
 
-  # Basic DFS to find all paths through the graph. There's a lot of
-  # string/array copying going on here which could be optimised by not
-  # referencing nodes with strings but this works and executes extremely
-  # quickly anyway. 
-  var totalPaths = 0
-  var stack = @[@["start"]]
-  while stack.len > 0:
-    let path = stack.pop()
-    if path[^1] == "end":
-      # Complete path found
-      inc totalPaths
-    else:
-      # Continue exploring path to adjacent big caves or not previously
-      # explored small caves
-      for n2 in g[path[^1]]:
-        if n2[0].isUpperAscii() or n2 notin path:
-          stack.add(path & n2)
-
-  echo "part 1: ", totalPaths
-  #echo "part 2: "
+  echo "part 1: ", part1(g)
+  echo "part 2: ", part2(g)
 
 when isMainModule:
   main()
