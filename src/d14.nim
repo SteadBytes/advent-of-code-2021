614408a484ea3b2fd5d495f9eea10dbf9adb9089
Ben Steadman <steadmanben1@gmail.com>
2021-12-14 20:37:51 +0000

    d14 p02
    Signed-off-by: Ben Steadman <steadmanben1@gmail.com>

--- src/d14.nim
+++ src/d14.nim
@@ -1,6 +1,8 @@
 import strutils, sequtils, tables, strscans
 
-type Pair = (char, char)
+type
+  Pair = (char, char)
+  Rules = Table[Pair, char]
 
 func parseRule(s: string): (Pair, char) =
   let (ok, a, b, insert) = s.scanTuple("$c$c -> $c")
@@ -12,7 +14,7 @@ func parseInput(s: string): (string, Table[Pair, char]) =
   assert sections.len == 2
   (sections[0], sections[1].splitLines().map(parseRule).toTable)
 
-func step(pairCounts: CountTable[Pair], rules: Table[Pair, char]): CountTable[Pair] =
+func step(pairCounts: CountTable[Pair], rules: Rules): CountTable[Pair] =
   for pair, n in pairCounts:
     if pair in rules:
       # AB -> C
@@ -22,20 +24,29 @@ func step(pairCounts: CountTable[Pair], rules: Table[Pair, char]): CountTable[Pa
       result.inc((pair[0], insert), n)
       result.inc((insert, pair[1]), n)
 
-proc main() =
-  const input = staticRead("../inputs/d14.txt")
-  let (tmpl, rules) = parseInput(input)
-  # As the final polymer string is not required, only the counts of each pair
-  # of elements need to be maintained
-  var pairCounts = zip(tmpl[0..^1], tmpl[1..^1]).toCountTable()
-  for _ in 0..<10:
+func insertionProcess(
+  pairCounts: CountTable[Pair],
+  rules: Rules,
+  rounds: int
+): int =
+  var pairCounts = pairCounts
+  for _ in 0..<rounds:
     pairCounts = step(pairCounts, rules)
   # Construct individual character counts from pair counts
   var counts: CountTable[char]
   for pair, n in pairCounts:
     counts.inc(pair[1], n)
-  echo "part 1: ", counts.largest[1] - counts.smallest[1]
-  #echo "part 2: "
+  counts.largest[1] - counts.smallest[1]
+
+proc main() =
+  const input = staticRead("../inputs/d14.txt")
+  let (tmpl, rules) = parseInput(input)
+  # As the final polymer string is not required, only the counts of each pair
+  # of elements need to be maintained
+  let pairCounts = zip(tmpl[0..^1], tmpl[1..^1]).toCountTable()
+  echo "part 1: ", insertionProcess(pairCounts, rules, 10)
+  echo "part 2: ", insertionProcess(pairCounts, rules, 40)
+
 
 when isMainModule:
   main()
