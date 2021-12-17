e4231167eed4ca8929a754770ea9318150060271
Ben Steadman <steadmanben1@gmail.com>
2021-12-17 13:30:10 +0000

    d15 p02
    Signed-off-by: Ben Steadman <steadmanben1@gmail.com>

--- src/d15.nim
+++ src/d15.nim
@@ -1,30 +1,52 @@
 import aoc
 import sequtils, sugar, heapqueue, tables, sets
 
-proc main() =
-  const input = staticRead("../inputs/d15.txt")
-  let grid = parseGrid(input)
+func findMinRiskPath(g: Grid[int]): int =
+  ## Dijkstra's Algorithm to find the shortest path using "risk level" as
+  ## cost/distance.
   let
     start = (0, 0)
-    target = (grid.width-1, grid.height-1)
+    target = (g.width-1, g.height-1)
 
-  var dist = {start: 0}.toTable()
-  var visited = [start].toHashSet()
-  var queue = {0: start}.toHeapQueue()
+  var
+    dist = {start: 0}.toTable()
+    visited = [start].toHashSet()
+    queue = {0: start}.toHeapQueue()
 
   while queue.len > 0:
     let (_, v) = queue.pop()
     if v == target:
-      echo "part 1: ", dist[target]
-      break
+      return dist[target]
     visited.incl(v)
-    for u in grid.neighbours(v).toSeq.filter(u => not visited.contains(u)):
-      let risk = dist[v] + grid[u]
+    for u in g.neighbours(v).toSeq.filter(u => not visited.contains(u)):
+      let risk = dist[v] + g[u]
       if risk < dist.getOrDefault(u, high int):
         dist[u] = risk
         queue.push((risk, u))
 
-  #echo "part 2: "
+proc main() =
+  const input = staticRead("../inputs/d15.txt")
+  let grid = parseGrid(input)
+
+  echo "part 1: ", findMinRiskPath(grid)
+
+  let
+    gridFactor = 5
+    maxRisk = 9
+    repeats = (0..<gridFactor).toSeq()
+    fullGrid = repeats.map(
+      yRep => grid.map(
+        row => repeats.map(
+          xRep => row.map(
+            # Wrap risk value into range 1..9
+            # The `maxRisk - 1` and `mod maxRisk + 1` terms ensures that risk
+            # values 1..8 are unchanged, whilst 9 is wrapped to 1
+            risk => (risk + yRep + xRep + maxRisk - 1) mod maxRisk + 1)
+        ).concat()
+      )
+    ).concat()
+
+  echo "part 2: ", findMinRiskPath(fullGrid)
 
 when isMainModule:
   main()
