5a0b152ca046a33fac55a7dc01d82377250d22d3
Ben Steadman <steadmanben1@gmail.com>
2021-12-20 11:00:36 +0000

    d18 p02
    I'm very glad almost all of the work for this part was in place from
    part 1!
    
    Signed-off-by: Ben Steadman <steadmanben1@gmail.com>

--- src/d18.nim
+++ src/d18.nim
@@ -130,17 +130,24 @@ func parseSnailfish(s: string): Node =
       return Node(kind: nkRegular, value: x)
   parse()
 
-proc parseInput(s: string): seq[Node] =
+func parseInput(s: string): seq[Node] =
   s.strip().splitLines.map(parseSnailfish)
 
-proc part1(numbers: seq[Node]): int =
+func part1(numbers: seq[Node]): int =
   numbers.foldl(a + b).magnitude
+
+func part2(numbers: seq[Node]): int =
+  for i, x in numbers:
+    for j, y in numbers:
+      if i == j:
+        continue
+      result = max(result, (x + y).magnitude)
 
 proc main() =
   const input = staticRead("../inputs/d18.txt")
   let numbers = parseInput(input)
   echo "part 1: ", part1(numbers)
-  #echo "part 2: "
+  echo "part 2: ", part2(numbers)
 
 when isMainModule:
   when defined(testing):
@@ -293,19 +300,22 @@ when isMainModule:
           magnitude(parseSnailfish("[[[[5,0],[7,4]],[5,5]],[6,6]]")) == 1137
           magnitude(parseSnailfish("[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]")) == 3488
 
+      let exampleInput = """[[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]
+        [[[5,[2,8]],4],[5,[[9,9],0]]]
+        [6,[[[6,2],[5,6]],[[7,6],[4,7]]]]
+        [[[6,[0,7]],[0,9]],[4,[9,[9,0]]]]
+        [[[7,[6,4]],[3,[1,3]]],[[[5,5],1],9]]
+        [[6,[[7,3],[3,2]]],[[[3,8],[5,7]],4]]
+        [[[[5,4],[7,7]],8],[[8,3],8]]
+        [[9,3],[[9,9],[6,[4,9]]]]
+        [[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]
+        [[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]]
+      """.unindent.parseInput()
       test "part 1":
-        let input = """[[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]
-          [[[5,[2,8]],4],[5,[[9,9],0]]]
-          [6,[[[6,2],[5,6]],[[7,6],[4,7]]]]
-          [[[6,[0,7]],[0,9]],[4,[9,[9,0]]]]
-          [[[7,[6,4]],[3,[1,3]]],[[[5,5],1],9]]
-          [[6,[[7,3],[3,2]]],[[[3,8],[5,7]],4]]
-          [[[[5,4],[7,7]],8],[[8,3],8]]
-          [[9,3],[[9,9],[6,[4,9]]]]
-          [[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]
-          [[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]]
-        """.unindent
-        check part1(parseInput(input)) == 4140
+        check part1(exampleInput) == 4140
+      test "part 2":
+        check part2(exampleInput) == 3993
+
 
   else:
     main()
